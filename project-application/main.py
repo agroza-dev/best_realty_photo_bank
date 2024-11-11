9c51d12965429b6f6abad5e08dca2d43094f6f15
a.groza <a.groza.dev@gmail.com>
2024-11-12 00:02:16 +0300

    Add config settings to run setup

--- project-application/main.py
+++ project-application/main.py
@@ -4,4 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 if __name__ == "__main__":
-    uvicorn.run("main.app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.app.host,
+        port=settings.app.port,
+        reload=True
+)
