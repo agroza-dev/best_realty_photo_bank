d2d8ea5534c777b1c5e217bc850aff469f257bf0
a.groza <a.groza.dev@gmail.com>
2025-04-11 22:54:23 +0300

    User model and schema for TG user

--- project-application/core/models/user.py
+++ project-application/core/models/user.py
@@ -1,8 +1,31 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import BigInteger, String, DateTime, Integer
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import func, text
 
 from .base import Base
 
 
 class User(Base):
-    username: Mapped[str] = mapped_column(unique=True)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
+    username: Mapped[str | None] = mapped_column(String(32), nullable=True)
+    first_name: Mapped[str | None] = mapped_column(String(64), nullable=True)
+    last_name: Mapped[str | None] = mapped_column(String(64), nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Integer, default=0, server_default=text("0"))
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        server_default=func.now(),
+        onupdate=lambda: datetime.now(timezone.utc),
+        server_onupdate=func.now(),
+    )
+
+    def __repr__(self) -> str:
+        return f"<User tg_id={self.telegram_id} username={self.username}>"
--- project-application/core/schemas/user.py
+++ project-application/core/schemas/user.py
@@ -1,13 +1,21 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    username: str
-
+    username: str | None
+    telegram_id: int
 
 class UserCreate(UserBase):
-    pass
+    first_name: str | None
+    last_name: str | None
 
 
 class UserRead(UserBase):
     id: int
+    first_name: str | None
+    last_name: str | None
+    is_deleted: int
+    created_at: datetime | None
+    updated_at: datetime | None
