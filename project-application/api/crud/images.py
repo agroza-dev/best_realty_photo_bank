e2241774ea594a3abeb30b532fecaf594de4373e
a.groza <a.groza.dev@gmail.com>
2025-04-17 16:44:54 +0300

    Added image CRUD

---
+++ project-application/api/crud/images.py
@@ -0,0 +1,35 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from typing import Sequence
+
+from core.models import Image
+from core.schemas.image import ImageUpdate
+from core.schemas.user import UserCreate
+
+
+async def get_all_images(session: AsyncSession) -> Sequence[Image]:
+    statement = select(Image).order_by(Image.id)
+    result = await session.scalars(statement)
+    return result.all()
+
+
+async def create_image(
+    session: AsyncSession,
+    image_create: UserCreate,
+) -> Image:
+    image = Image(**image_create.model_dump())
+    session.add(image)
+    await session.commit()
+    return image
+
+
+async def update_image(
+    session: AsyncSession,
+    image: Image,
+    image_update: ImageUpdate
+) -> Image:
+    for name, value in image_update.model_dump(exclude_unset=True).items():
+        setattr(image, name, value)
+    await session.commit()
+    return image
--- project-application/core/models/__init__.py
+++ project-application/core/models/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "Image",
 )
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .image import Image
