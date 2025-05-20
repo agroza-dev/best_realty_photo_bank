59c0bdca1a07376b51c0d4e0fd768c06042a0a41
a.groza <a.groza.dev@gmail.com>
2025-05-21 01:43:42 +0300

    Added templates filter for styles cache refresh

--- project-application/utils/templates.py
+++ project-application/utils/templates.py
@@ -1,7 +1,21 @@
-import re
+import os
+import random
+from pathlib import Path
 import jinja2
 from typing import Optional
 from core.config import settings
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+def autoversion_filter(filepath: str) -> str:
+    static_file = Path(settings.web_app.templates) / filepath.replace('/templates', '').lstrip("/")
+    try:
+        mtime = int(os.path.getmtime(static_file))
+
+        return f"{filepath}?v={mtime}"
+    except FileNotFoundError:
+        return f"{filepath}?v={random.randint(1, 10)}"
 
 
 def render_common_template(template_name: str, data: Optional[dict] = None) -> str:
@@ -40,6 +54,8 @@ def _get_template_env(template_path: str) -> jinja2.Environment:
             lstrip_blocks=True,
             autoescape=True,
         )
+        env.filters["autoversion"] = autoversion_filter
+
         _get_template_env.template_envs[template_path] = env
 
     return _get_template_env.template_envs[template_path]
